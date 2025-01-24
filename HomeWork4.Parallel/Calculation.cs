5a8eb86e4a0bb3a90484603d01a81247d960ddf4
Marat <milya010790@mail.ru>
2025-01-24 14:57:34 +0500

    Добавлено подсчет суммы  последовательно

---
+++ HomeWork4.Parallel/Calculation.cs
@@ -0,0 +1,16 @@
+﻿using System.Numerics;
+
+namespace HomeWork4.Parallel;
+internal class Calculation
+{
+    public static T SequentialSum<T>(ICollection<T> list) where T : struct, INumber<T>
+    {
+        T sum = default;
+        foreach (T item in list)
+        {
+            checked { sum += item; }
+        }
+        return sum;
+    }
+}
+
