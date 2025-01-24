ca70c0c1cd51ca9b3054f228b9df9fd93c37ba87
Marat <milya010790@mail.ru>
2025-01-24 14:36:51 +0500

    Добавлен логгер

---
+++ HomeWork4.Parallel/Logger.cs
@@ -0,0 +1,29 @@
+﻿namespace HomeWork4.Parallel;
+public static class Logger
+{
+    /// <summary>Запись в консоль красным цветом заголовок</summary>
+    /// <param name="titleText">текст</param>
+    public static void WriteTitle(string titleText)
+    {
+        var consoleColor = Console.ForegroundColor;
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(titleText);
+        Console.ForegroundColor = consoleColor;
+    }
+
+    /// <summary>
+    /// Запись в консоль с выделением
+    /// </summary>
+    /// <param name="statement">Смысловое выражение</param>
+    /// <param name="addition">Дополнение</param>
+    public static void WriteDescription(string statement, string addition)
+    {
+        var consoleColor = Console.ForegroundColor;
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.Write($"{statement}:  ");
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.Write($"{addition}  \n");
+        Console.ForegroundColor = consoleColor;
+    }
+}
+
