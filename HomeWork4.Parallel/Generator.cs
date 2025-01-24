51ed72177dd896153d914610feb451e1c3611c92
Marat <milya010790@mail.ru>
2025-01-24 14:37:26 +0500

    Добавлен генератор коллекций

---
+++ HomeWork4.Parallel/Generator.cs
@@ -0,0 +1,20 @@
+﻿namespace HomeWork4.Parallel;
+public class Generator
+{
+    /// <summary>Генератор коллекции с заполнением случайными числами </summary>
+    /// <param name="size">размер коллекции</param>
+    /// <returns>Заполненная коллекция с заданным размером </returns>
+    public static ICollection<int> GenerateList(int size)
+    {
+        Random random = new();
+
+        ICollection<int> list = [];
+        for (int i = 0; i < size; i++)
+        {
+            int randomNumber = random.Next(1, 101);
+            list.Add(randomNumber);
+        }
+        return list;
+    }
+}
+
