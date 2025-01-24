bbce5bd7f967c6b029a2b3c1387437bcce086da0
Marat <milya010790@mail.ru>
2025-01-24 15:03:05 +0500

    описание

--- HomeWork4.Parallel/Calculation.cs
+++ HomeWork4.Parallel/Calculation.cs
@@ -3,6 +3,11 @@
 namespace HomeWork4.Parallel;
 internal class Calculation
 {
+
+    /// <summary>Расчет суммы коллекции последовательно</summary>
+    /// <typeparam name="T">Тип</typeparam>
+    /// <param name="list">Коллекция</param>
+    /// <returns>Возврат суммы</returns>
     public static T SequentialSum<T>(ICollection<T> list) where T : struct, INumber<T>
     {
         T sum = default;
